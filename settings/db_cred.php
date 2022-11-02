1a331c8ce9de66cfd96113c1aac152fe71c23bc4
kwame.ofori-gyau@ashesi.edu.gh <kwame.ofori-gyau@ashesi.edu.gh>
2022-11-02 17:57:30 +0000

    fin

--- settings/db_cred.php
+++ settings/db_cred.php
@@ -2,17 +2,14 @@
 //Database credentials
 
 // syntax for creating a constant variable in php
-    // define('SERVERNAME','localhost');
-    // define('HOSTNAME','root');
-    // define('PASSWORD','');
-    // define('DATABASE','shoppn');
+    define('SERVERNAME','localhost');
+    define('HOSTNAME','root');
+    define('PASSWORD','');
+    define('DATABASE','shoppn');
 
 
-    define('SERVERNAME','us-cdbr-east-06.cleardb.net');
-    define('HOSTNAME','b345c0e5c925f0');
-    define('PASSWORD','3a6ff711');
-    define('DATABASE','heroku_54c070a3bc39030');
-
-
-
+    // define('SERVERNAME','us-cdbr-east-06.cleardb.net');
+    // define('HOSTNAME','b345c0e5c925f0');
+    // define('PASSWORD','3a6ff711');
+    // define('DATABASE','heroku_54c070a3bc39030');
 ?>
